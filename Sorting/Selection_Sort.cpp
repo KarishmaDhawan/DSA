a1555a5926ce568432e8a407d231dde35c3bd3d6
KarishmaDhawan <karishmadhawan20@gmail.com>
2024-02-03 17:21:33 +0530

    Create Selection_Sort.cpp

---
+++ Sorting/Selection_Sort.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+void selection_sort(int a[],int n){
+    int i,j,temp,min;
+    for(i=0;i<n-1;i++){
+        min=a[i];
+        for(j=i+1;j<n;j++){
+            if(a[j]<min){
+                temp=a[j];
+                a[j]=min;
+                min=temp;
+            }
+            a[i]=min;
+        }
+    }      
+}
+int main(){
+    int n;
+    cout<<"Enter the size of array: "<<endl;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    selection_sort(a,n);
+    cout<<"Sorted Array is: ";
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";
+    }
+    return 0;
+}
